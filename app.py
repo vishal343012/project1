cb44c90040bc209fcfd70025e468645f714d8944
vishal343012 <119778329+vishal343012@users.noreply.github.com>
2023-12-28 15:12:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def hello3():
     html = "<h3>Hello World  {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    return html.format(name=os.getenv("NAME"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
